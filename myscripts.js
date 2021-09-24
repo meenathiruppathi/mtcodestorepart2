7acfd7e0608448e53e24fe377bc391123514184b
meenathiruppathi <91307967+meenathiruppathi@users.noreply.github.com>
2021-09-24 11:49:51 +0530

    Add files via upload

--- myscripts.js
+++ myscripts.js
@@ -1,7 +1,48 @@
+
+// jquery selectors
+
 $(document).ready(function(){
 	alert('hlo');
-  $("button").click(function(){
-	  alert('hlo');
+  $("#button").click(function(){
     $("#test").hide();
+	$(".newclass").hide();
+	$("p:first").hide();
+	// $("p:last").append('This is last p tag div);
+	//$('div').append('This is div.);
+	 $(this).css("backgroundColor", "grey");
+	  $("text").hide();
+	  $("file").hide();
   });
+  
+  
+  $("#fbutton").click(function(){
+    $("#test").show();
+	$(".newclass").show();
+	$("p:first").show();
+	// $("p:last").append('This is last p tag div);
+	//$('div').append('This is div.);
+	 $(this).css("backgroundColor", "Red");
+	 var element;
+	     element=  document.querySelector(".newclass");
+		// element.innerHTML="change in content";
+		// element="hey";
+		// alert(element);
+		
+ $(":text").show();
+	  $(":file").show();
+ 
+	 
+  });
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 });
